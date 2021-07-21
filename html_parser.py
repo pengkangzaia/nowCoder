7dbbe3517b91a5b2de0dbc127a9b0a52a1402729
camille <pengkangzaia@foxmail.com>
2021-07-21 08:32:10 +0800

    更新登录逻辑

--- html_parser.py
+++ html_parser.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
 
 # webDriver应用程序路径
 driverPath = r"D:\project\learn\python\chromedriver.exe"
@@ -7,32 +9,37 @@ wd.implicitly_wait(5)
 
 # 先登录
 wd.get("https://www.nowcoder.com/login")
-email_phone_input = wd.find_element_by_id("jsEmailIpt")
-email_phone_input.send_keys("你的账号")
-password_input = wd.find_element_by_id("jsPasswordIpt")
-password_input.send_keys("密码")
-login_button = wd.find_element_by_id("jsLoginBtn")
+tab_items = wd.find_elements_by_css_selector(".tab-item")
+tab_items[1].click()
+phone_password_input = wd.find_elements_by_css_selector(".el-input__inner")
+phone_password_input[0].send_keys("15298372242")
+phone_password_input[1].send_keys("qwe951951")
+login_button = wd.find_element_by_css_selector(".el-button")
 login_button.click()
+after_login = wd.current_window_handle
 
 # 打开URL文件
 split = '===================================面经分割线==================================\n'
 prefix = 'https://www.nowcoder.com'
-for line in open("./url_almost_full.txt"):
+for line in open("./url_20210720.txt"):
     # 跳转到需要访问的页面
     line = line.replace('\n', '').replace('\r', '')
     print(prefix + line)
     script = 'window.open("' + prefix + line + '")'
-    new_handle = wd.execute_script(script)
-    main_handle = wd.current_window_handle
+    wd.execute_script(script)
     handles = wd.window_handles
     for handle in handles:
-        # 关闭登录跳转页面，转到面经页面
-        if handle == main_handle:
-            wd.close()
-        else:
+        if handle != after_login:
             wd.switch_to.window(handle)
+            break
     element = wd.find_element_by_css_selector(".post-topic-des")
-    with open('./questions.txt', 'a', encoding='utf-8') as f:
+    title = wd.find_elements_by_css_selector(".post-title")
+    with open('./questions_20210720.txt', 'a', encoding='utf-8') as f:
+        f.write('==============================' + str(title[0].text) + '===========================================\n')
         f.write(str(element.text) + '\n')
-        f.write(split)
+        f.write('============================================面经分割线===============================================\n')
+    print(title[0].text)
     print(element.text)
+    wd.close()
+    wd.switch_to_window(after_login)
+    # ActionChains(wd).key_down(Keys.CONTROL).send_keys("w").key_up(Keys.CONTROL).perform()
--- url_parser.py
+++ url_parser.py
@@ -11,13 +11,13 @@ wd.implicitly_wait(5)
 
 
 # 如果不能登录，先看看网络通不通。检查本地wifi，ping牛客网等等
-wd.get("https://www.nowcoder.com/login")
-email_phone_input = wd.find_element_by_id("jsEmailIpt")
-email_phone_input.send_keys("你的账号")
-password_input = wd.find_element_by_id("jsPasswordIpt")
-password_input.send_keys("你的密码")
-login_button = wd.find_element_by_id("jsLoginBtn")
-login_button.click()
+# wd.get("https://www.nowcoder.com/login")
+# email_phone_input = wd.find_element_by_id("jsEmailIpt")
+# email_phone_input.send_keys("15298372242")
+# password_input = wd.find_element_by_id("jsPasswordIpt")
+# password_input.send_keys("qwe951951")
+# login_button = wd.find_element_by_id("jsLoginBtn")
+# login_button.click()
 
 # Java精选面经合集地址：https://www.nowcoder.com/discuss/experience?tagId=639
 # 先获取到所有帖子的访问URL
@@ -32,19 +32,19 @@ for handle in handles:
         wd.switch_to.window(handle)
 
 # 滚动下拉动态获取帖子，目前只能拉取100页数据
-page_num = 99
+page_num = 150
 for i in range(page_num):
     # 刷新下一个响应
     # x=0,y=document.body.scrollHeight
     wd.execute_script("window.scrollTo(0, document.body.scrollHeight)")
     ActionChains(wd).key_down(Keys.END).perform()
-    time.sleep(5)
+    time.sleep(2)
 
 # 获取URL
 elements = wd.find_elements_by_xpath("//ul[@class='column-best-list']/li")
 for element in elements:
     # 追加存入文件中
-    with open('./url.txt', 'a', encoding='utf-8') as f:
+    with open('./url_20210721.txt', 'a', encoding='utf-8') as f:
         f.write(str(element.get_attribute("data-href")) + '\n')
 
 
